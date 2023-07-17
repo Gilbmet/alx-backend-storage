ca11f7bd9510be79755a243417848560dcb0e391
Gilbmet <gilbmet254@gmail.com>
2023-07-17 19:40:30 +0300

    Update 12-log_stats.py

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -31,6 +31,7 @@ def log_stats(mongo_collection, option=None):
     status_check = mongo_collection.count_documents({"method": "GET", "path": "/status"})
     print(f"{status_check} status check")
 
+
 if __name__ == "__main__":
     client = MongoClient('mongodb://127.0.0.1:27017')
     logs_db = client.logs
